c8d4308b024dfd51d898a7c03995e0afa70ac10b
Tabitha <flor.palida@yahoo.com>
2023-06-19 21:06:40 -0400

    corrected errors

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// function scuberGreetingForFeet(someNumber){
+//  if (someNumber <= 400) {
+//   return "This one is on me!";
+//  }
+// }
+// function scuberGreetingForFeet(someNumber) {
+
+//   if (someNumber <= 400){
+//           return "This one is on me!";
+//   }
+//   else if (someNumber > 2000 && someNumber < 2500){
+//       return "I will gladly take your thirty bucks.";
+//   }
+//   else if(someNumber > 2500){
+//       return "No can do.";
+//   }
+//   else if(someNumber > 400 > 2000){
+//     return "I will gladly take your twenty dollars.";
+
+// }
+function scuberGreetingForFeet(someNumber) {
+
+  if (someNumber <= 400){
+          return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+      return "No can do.";
+  }
+  else if(someNumber > 400 > 2000){
+    return "I will gladly take your twenty dollars.";
+  }
+  else if(someNumber <= 400 <= 2000){
+    return "That will be twenty bucks.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous"){
+      return "Thank you.";
+    } else {
+      return "Bye.";
+    }
+    }
